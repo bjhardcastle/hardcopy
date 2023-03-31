c6c4164cc96f4e0a7841c4346f47ce4a8c3db680
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-03-30 22:06:08 -0700

    Re-organize abc and add concurrency

--- src/hardcopy/validate.py
+++ src/hardcopy/validate.py
@@ -3,11 +3,14 @@ from __future__ import annotations
 import abc
 import asyncio
 import concurrent.futures
+import contextlib
 import doctest
+import logging
 import os
 import pathlib
 import sys
 import tempfile
+from typing import Callable, ClassVar, Optional
 
 if sys.version_info >= (3, 11):
     from enum import StrEnum
@@ -17,6 +20,8 @@ else:
 import crc32c
 
 from hardcopy.types import PathLike
+
+logger = logging.getLogger(__name__)
 
 
 class Algorithms(StrEnum):
@@ -33,42 +38,85 @@ class Algorithms(StrEnum):
 class DataValidater(abc.ABC):
     """Abstract base class for validating data integrity."""
 
+    algorithm: str
+
+    def get_checksum(self, path: PathLike) -> str | int:
+        """Return a checksum of the data at `path`."""
+        return NotImplemented
+
+    def is_valid_copy(self, src: PathLike, *copy: PathLike) -> bool:
+        """Every `copy` has identical data to `src`."""
+        return NotImplemented
+
+    def is_valid_copytree(self, src_dir: PathLike, copy_dir: PathLike) -> bool:
+        """Every file in `src_dir` has identical data at same relative path in
+        `copy_dir`.
+        """
+        return NotImplemented
+
+
+class ConcurrentValidater(DataValidater):
+
     algorithm: str = Algorithms.CRC32C
 
+    _threadpool: ClassVar[concurrent.futures.ThreadPoolExecutor]
+    _processpool: ClassVar[concurrent.futures.ProcessPoolExecutor]
+
+    @property
+    def threadpool(self) -> concurrent.futures.ThreadPoolExecutor:
+        with contextlib.suppress(AttributeError):
+            return self._threadpool
+        self.__class__._threadpool = concurrent.futures.ThreadPoolExecutor()
+        return self.threadpool
+
+    @property
+    def processpool(self) -> concurrent.futures.ProcessPoolExecutor:
+        with contextlib.suppress(AttributeError):
+            return self._processpool
+        self.__class__._processpool = concurrent.futures.ProcessPoolExecutor()
+        return self.processpool
+
     @staticmethod
-    @abc.abstractmethod
-    def get_checksum(path: PathLike) -> str | int:
+    def checksum_in_chunks(
+        fn: Callable[[bytes], str | int], path: pathlib.Path
+    ) -> str | int:
+        """Return a checksum of the data at `path`, breaking into chunks if necessary."""
+        return fn(path.read_bytes())
+
+    def get_checksum(self, path: PathLike) -> str | int:
         """Return a checksum of the data at `path`.
 
         >>> tmpfile = tempfile.NamedTemporaryFile(delete=False).name
-        >>> bool(DataValidater.get_checksum(tmpfile))
+        >>> bool(ConcurrentValidater().get_checksum(tmpfile))
         False
         >>> _ = pathlib.Path(tmpfile).write_text('test')
-        >>> bool(DataValidater.get_checksum(tmpfile))
+        >>> bool(ConcurrentValidater().get_checksum(tmpfile))
         True
         """
-        return crc32c.crc32c(pathlib.Path(os.fsdecode(path)).read_bytes())
+        path = pathlib.Path(os.fsdecode(path))
+        future = self.processpool.submit(
+            self.checksum_in_chunks, crc32c.crc32c, path
+        )
+        return future.result()
 
-    @classmethod
-    def is_valid_copy(cls, src: PathLike, *copy: PathLike) -> bool:
+    def is_valid_copy(self, src: PathLike, *copy: PathLike) -> bool:
         """Every `copy` has identical data to `src`.
 
         >>> tmpfile = tempfile.NamedTemporaryFile(delete=False).name
-        >>> DataValidater.is_valid_copy(tmpfile, tmpfile, tmpfile)
+        >>> ConcurrentValidater().is_valid_copy(tmpfile, tmpfile, tmpfile)
         True
         >>> tmpfile2 = pathlib.Path(tmpfile).with_suffix('.2')
         >>> _ = tmpfile2.write_text('test')
-        >>> DataValidater.is_valid_copy(tmpfile, tmpfile, tmpfile2)
+        >>> ConcurrentValidater().is_valid_copy(tmpfile, tmpfile, tmpfile2)
         False
         """
         src = pathlib.Path(os.fsdecode(src))
         if src.is_dir():
-            return cls.is_valid_copytree(src, *copy)
-        target = cls.get_checksum(src)
-        return all(cls.get_checksum(path) == target for path in copy)
+            return self.is_valid_copytree(src, *copy)
+        target = self.get_checksum(src)
+        return all(self.get_checksum(path) == target for path in copy)
 
-    @classmethod
-    def is_valid_copytree(cls, src_dir: PathLike, copy_dir: PathLike) -> bool:
+    def is_valid_copytree(self, src_dir: PathLike, copy_dir: PathLike) -> bool:
         """`copy_dir` has identical data to `src_dir`.
 
         Only checks contents of `src_dir`: extra files in `copy_dir` are
@@ -76,29 +124,44 @@ class DataValidater(abc.ABC):
 
         >>> tmpdir = tempfile.TemporaryDirectory(suffix='test').name
         >>> tmpfile = tempfile.NamedTemporaryFile(delete=False).name
-        >>> DataValidater.is_valid_copytree(tmpdir, tmpdir)
+        >>> ConcurrentValidater().is_valid_copytree(tmpdir, tmpdir)
         True
-        >>> DataValidater.is_valid_copytree(pathlib.Path(tmpfile).parent, tmpdir)
+        >>> ConcurrentValidater().is_valid_copytree(pathlib.Path(tmpfile).parent, tmpdir)
         False
         """
         src_dir = pathlib.Path(os.fsdecode(src_dir))
         copy_dir = pathlib.Path(os.fsdecode(copy_dir))
-        futures = []
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            for src_path in src_dir.rglob('*'):
-                copy_path = copy_dir / src_path.relative_to(src_dir)
-                futures.append(
-                    executor.submit(copy_path.exists)
-                )   # dirs only checked for existence
-                futures.append(
-                    executor.submit(cls.is_valid_copy, src_path, copy_path)
-                )
-                if any(
-                    future.result() == False
-                    for future in concurrent.futures.as_completed(futures)
-                ):
-                    return False
-        return True
+        future_to_path: dict[concurrent.futures.Future, pathlib.Path] = {}
+        for src_path in src_dir.glob('*'):
+            copy_path = copy_dir / src_path.relative_to(src_dir)
+            if not copy_path.exists():
+                bad_path = copy_path
+                break
+            # dirs only checked for existence
+            future_to_path.update(
+                {
+                    self.threadpool.submit(
+                        self.is_valid_copy, src_path, copy_path
+                    ): src_path
+                }
+            )
+        else:
+            for future in concurrent.futures.as_completed(future_to_path):
+                if not future.result():
+                    bad_path = future_to_path[future]
+                    break
+            else:
+                return True
+
+        for future in future_to_path:
+            future.cancel()
+
+        if bad_path.exists():
+            logger.debug(f'{bad_path} is not a valid copy')
+        else:
+            logger.debug(f'{bad_path} does not exist')
+
+        return False
 
 
 if __name__ == '__main__':
